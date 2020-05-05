61724503311790433bd5140e2e9a2b2dd1257fdd
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-05 20:10:42 +0100

    fun esoteric book page start

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,16 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     function lineWidthExpand() {
         $(".main_title").animate({
             opacity: "1",
         })
         $(".lineWidth").delay("1000").animate({
-           width: "10%",
-           opacity: "1",
-        },1000);
+            width: "10%",
+            opacity: "1",
+        }, 1000);
 
     }
-
     window.onload = lineWidthExpand();
-
-
 
 
     $(".author_1").click(show_AlanWatts_Info);
@@ -24,9 +21,9 @@ $(document).ready(function() {
             width: "100%",
             height: "100%",
             borderRadius: "0",
-        },350, "linear")
-        $(".authors").css("color", "white");
+        })
     }
+
 
 
 })
